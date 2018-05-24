4cc421b7625a5d9751c57cabd7d60fb9170ef2a6
plamenti <plamenti_@abv.bg>
2018-05-24 15:24:04 +0300

    Implement proofOfWork method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -83,4 +83,26 @@ Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, n
     return hash;
 }
 
+/**
+ * Very Important method!
+ * Takes currentBlockData and tries to generate a specific hash
+ * Will repeatedly hash previousBlockHash and currentBlockData and nonce util it get an acceptable hash generated 
+ * that starts for example with 0000 - e.g. 0000ASASTATTDATEHBBDA637DBBBSHHNNSJJJSJDJS72733
+ * 
+ * @param {*} previousBlockHash 
+ * @param {*} currentBlockData 
+ * 
+ * @returns The nonce value that creates the correct hash
+ */
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData){
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    while(hash.substring(0,4) != '0000'){
+        nonce++;
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    }
+
+    return nonce;
+}
+
 module.exports = Blockchain;
