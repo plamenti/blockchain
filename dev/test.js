5743113af7ab47a99b55622ec4e14a4d26b193f0
plamenti <plamenti_@abv.bg>
2018-05-24 11:13:33 +0300

    Add some dummy data to test

--- dev/test.js
+++ dev/test.js
@@ -2,4 +2,8 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
+bitcoin.createNewBlock(2389, "UIDUD98798HJFD", "HJDHKLAIOEIRH677")
+bitcoin.createNewBlock(213, "12DUDSDS98HJFD", "KSHDJSHd565666")
+bitcoin.createNewBlock(4334, "09SDSDSUUIWEUU", "NMNMSD8787SDDSD")
+
 console.log(bitcoin);
